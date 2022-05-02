3de4fc56d38d343b6fa47cf1943edf136b3cfc4b
Benjamin Matschke <benjaminmatschke@googlemail.com>
2022-05-02 19:01:03 +0300

    added preliminary schedule

--- src/schedule.php
+++ src/schedule.php
@@ -30,92 +30,90 @@
   }
 ?>
 
+<div style="float: right; padding-top: 1.5em;">
+<div class="cleanslate w24tz-current-time w24tz-small" style="display: inline-block !important; visibility: hidden !important; min-width:200px !important; min-height:100px !important;"><p><a href="//24timezones.com/Boston/time" style="text-decoration: none" class="clock24" id="tz24-1651501819-c143-eyJob3VydHlwZSI6IjEyIiwic2hvd2RhdGUiOiIxIiwic2hvd3NlY29uZHMiOiIwIiwiY29udGFpbmVyX2lkIjoiY2xvY2tfYmxvY2tfY2I2MjZmZWFmYjU0OTRkIiwidHlwZSI6ImRiIiwibGFuZyI6ImVuIn0=" title="World Time :: Boston" target="_blank" rel="nofollow">Current time at workshop (EDT, Boston)</a></p><div id="clock_block_cb626feafb5494d"></div></div>
+</div>
 
-<h3>Schedule</h3>
 
-The schedule will become available during the first week of May.
+<h3>Schedule (preliminary)</h3>
+
+
+<script type="text/javascript" src="//w.24timezones.com/l.js" async></script>
+
+<!--The schedule will become available during the first week of May.
 
 <p>
 <p>
+-->
 
-<!--
 For the titles, move the mouse over the names of the speakers.
 <br>
 For the <a href="abstracts.php">abstracts</a>, click on their names.
 <br>
-There is a <a href="schedule.pdf">printable version of the schedule</a>.
+<!--There is a <a href="schedule.pdf">printable version of the schedule</a>.
 <p>
-New: Some speakers kindly provided their slides, see below.
-<p>
-
-<div class="schedule_day">
-Wednesday June 7
-</div>
-
-<table>
-<?php
-  entry('9:00','9:10','Opening','text');
-  entry('9:10','10:00','Rafael von K&auml;nel','min50','abstracts.php#RK','On Mordell equations');
-  entry('10:00','10:30','Coffee','text');
-  entry('10:30','11:20','Samir Siksek','min50','abstracts.php#SS','Frey Curves, the Large Sieve and a Problem of Erd&#337;s');
-  entry('11:30','11:50','Vandita Patel','min20','abstracts.php#VP','Sums of consecutive perfect powers is seldom a perfect power','patel.pdf');
-  entry('12:00','13:50','Lunch','text');
-  entry('14:00','14:30','Samuele Anni','min30','abstracts.php#SA','Diophantine equations and semistable elliptic curves over totally real fields','anni.pdf');
-  entry('14:40','15:00','George Turcas','min20','abstracts.php#GT',"Fermat's Last Theorem over Quadratic Imaginary Fields of Class Number one. An overview of the difficulties.");
-  entry('15:00','15:30','Coffee','text');
-  entry('15:30','16:00','Francesco Veneziano','min30','abstracts.php#FV',"Unlikely intersections and the effective Mordell&ndash;Lang conjecture");
-  entry('16:10','16:30','Matija Kazalicki','min20','abstracts.php#MK','Supersingular zeros of divisor polynomials of elliptic curves of prime conductor');
-  entry('16:35','16:55','Filip Najman','min20','abstracts.php#FN','Torsion of elliptic curves over number fields','najman.pdf');
-?>
-</table>
-
-<p>
-The lunch on Wednesday takes place at <a href="practicalInformation.php#lunch">Bistro R&eacute;gent</a>.
-
-<p>
-
-<div class="schedule_day">
-Thursday June 8
-</div>
-
-<table>
-<?php
-  entry('9:00','9:50','Michael Stoll','min50','abstracts.php#MS','Rational points on curves in practice');
-  entry('9:50','10:20','Coffee','text');
-  entry('10:20','11:10','Jennifer S. Balakrishnan','min50','abstracts.php#JB','p-adic heights and rational points on curves','balakrishnan.pdf');
-  entry('11:20','11:50','Steffen M&uuml;ller','min30','abstracts.php#SM','Quadratic Chabauty for hyperelliptic curves with RM Jacobian');
-  entry('12:00','13:50','Lunch','text');
-  entry('14:00','14:30','Florian Luca','min30','abstracts.php#FL','On Diophantine quadruples of Fibonacci numbers','luca.pdf');
-  entry('14:40','15:00','Bernadette Faye','min20','abstracts.php#BF','Power of Two as Sums of Three Pell Numbers','faye.pdf');
-  entry('15:00','15:30','Coffee','text');
-  entry('15:30','15:50','Kwok Chi Chim','min20','abstracts.php#KCC','A lower bound for linear forms in two p-adic logarithms');
-  entry('15:55','16:15','Akinari Hoshi','min20','abstracts.php#AH','Cubic Thue equations and simplest cubic fields');
-  entry('16:20','16:40','David Corwin','min20','abstracts.php#DC','Motivic Periods and Coleman Functions','corwin.pdf');
-?>
-<tr><td colspan="3" align="right">20:00</td><td></td><td class="text">Conference Dinner</td></tr>
-</table>
-
-<p>
-The lunch on Thursday takes place at the math department.
-<br>
-The conference dinner takes place at <a href="practicalInformation.php#conferenceDinner">Caf&eacute; Maritime</a>.
-
-<p>  
-
-<div class="schedule_day">
-Friday June 9
-</div>
-
-<table>
-<?php
-  entry('9:30','10:20','Jean-Fran&ccedil;ois Mestre','min50','abstracts.php#JFM','Isog&eacute;nies entre jacobiennes de courbes hyperelliptiques');
-  entry('10:20','10:50','Coffee','text');
-  entry('10:50','11:40','Hendrik W. Lenstra','min50','abstracts.php#HL','Solving equations in orders');
-  entry('11:40','11:50','Closing','text');
-?>
-</table>
-
 -->
+<!--New: Some speakers kindly provided their slides, see below.
+-->
+<p>
+
+
+<div class="schedule_day">
+Saturday, May 21
+</div>
+
+<style>
+sup.white {
+    color: #fff;
+}
+sup.gray {
+    color: #888;
+}
+</style>
+
+<table>
+<?php
+  entry('9:50','10:00<sup class="gray">AM</sup>','Opening','text');
+  entry('10:00','10:20<sup class="white">AM</sup>','Simon Plouffe','min20','abstracts.php#SP','TBA');
+  entry('10:30','10:50<sup class="white">AM</sup>','Alexander M Kasprzyk, Gavin Brown','min20','abstracts.php#AK_GB','TBA');
+  entry('11:00','11:30<sup class="white">AM</sup>','Coffee','text');
+  entry('11:30','11:50<sup class="white">AM</sup>','Urs Schreiber','min20','abstracts.php#US','The nLab project &ndash; past and possible future');
+  entry('12:00','12:20<sup class="gray">PM</sup>','Fredrik Johansson','min20','abstracts.php#FJ','Fungrim: a semantic library of mathematical functions');
+  entry('12:30',' 1:30<sup class="white">PM</sup>','Lunch','text');
+  entry(' 1:30',' 1:50<sup class="white">PM</sup>','Kevin M. Buzzard','min20','abstracts.php#KB','mathlib: a database of mathematical proofs');
+  entry(' 2:00',' 2:20<sup class="white">PM</sup>','Alex J. Best','min20','abstracts.php#AB','Mathematical results as structured data');
+  entry(' 2:30',' 3:00<sup class="white">PM</sup>','Coffee','text');
+  entry(' 3:00',' 3:20<sup class="white">PM</sup>','William Stein','min20','abstracts.php#WS','Ways to Share Mathematical Computations and Data using CoCalc');
+  entry(' 3:30',' 4:00<sup class="white">PM</sup>','Open discussion','text');
+?>
+</table>
+
+<p>
+<!--
+The lunch on Wednesday takes place at <a href="practicalInformation.php#lunch">Bistro R&eacute;gent</a>.
+<p>
+-->
+
+<div class="schedule_day">
+Sunday, May 22
+</div>
+
+<table>
+<?php
+  entry('10:00','10:20<sup class="gray">AM</sup>','Jan Goedgebeur','min20','abstracts.php#JG','House of Graphs: a database of interesting graphs');
+  entry('10:30','10:50<sup class="white">AM</sup>','Hadrien Mélot','min20','abstracts.php#HM','TBA');
+  entry('11:00','11:30<sup class="white">AM</sup>','Coffee','text');
+  entry('11:30','11:50<sup class="white">AM</sup>','Martin Rubey','min20','abstracts.php#MR','FindStat');
+  entry('12:00','12:20<sup class="gray">PM</sup>','Andreas Paffenholz','min20','abstracts.php#','polyDB: A database for geometric objects');
+  entry('12:30',' 1:30<sup class="white">PM</sup>','Lunch','text');
+  entry(' 1:30',' 1:50<sup class="white">PM</sup>','Edgar Costa','min20','abstracts.php#EC_DR','The L-functions and modular forms database 1');
+  entry(' 2:00',' 2:20<sup class="white">PM</sup>','David Roe','min20','abstracts.php#EC_DR','The L-functions and modular forms database 2');
+  entry(' 2:30',' 3:00<sup class="white">PM</sup>','Coffee','text');
+  entry(' 3:00',' 3:20<sup class="white">PM</sup>','Andrew V. Sutherland','min20','abstracts.php#AS','Enumerating mathematical objects via cloud computing');
+  entry(' 3:30',' 4:00<sup class="white">PM</sup>','Stephen Miller','min20','abstracts.php#SM','TBA');
+?>
+</table>
+
 
 <p>
 
