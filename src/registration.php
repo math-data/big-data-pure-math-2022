5f452cc329591c8f35033e228456233ac67273c2
Benjamin Matschke <benjaminmatschke@googlemail.com>
2022-05-03 15:28:01 +0300

    updated deadline for registration

--- src/registration.php
+++ src/registration.php
@@ -5,8 +5,8 @@
   include "header.php";
 ?>
 
-The deadline for registration is April 30.
-Soon after the scientific committee will meet concerning the short talks. <!-- and funding requests.-->
+The deadline for registration is May 14.
+<!--Soon after the scientific committee will meet concerning the short talks. --><!-- and funding requests.-->
 <p>
 
 <iframe width="100%" height="1650em" frameborder="0" src="https://docs.google.com/forms/d/e/1FAIpQLSeaJvXzQRZ31CRngbdFNXlrRNb9kByy_gnAYK-HVVCgys08Jg/viewform?embedded=true" width="640" height="1350" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
